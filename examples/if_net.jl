d3888c4b919a8d0d9591dfd91e4ebfe751a95f4a
Russell Jarvis <russelljarvis@protonmail.com>
2023-03-13 16:48:02 +1100

    Update if_net.jl

--- examples/if_net.jl
+++ examples/if_net.jl
@@ -16,40 +16,12 @@ function makeNetGetTimes()
     SNN.monitor([E,I], [:fire])
     @time SNN.sim!(P, C; duration = 0.2second)
     print("simulation done !")
-    SNN.raster([E,I]) #|> display
-    Plots.savefig("cheap_dirty_plot.png")
-
-
     (times,nodes) = SNN.get_trains([E,I])
-    
     @time o1 = HeatMap(zip(minimum(times):maximum(times)/100.0:maximum(times),minimum(nodes):maximum(nodes/100.0):maximum(nodes)) )
     @time fit!(o1,zip(times,convert(Vector{Float64},nodes)))
     plot(o1, marginals=false, legend=true) #|>display 
     Plots.savefig("default_heatmap.png")
-
     return (P,C,times,nodes)
 end
-#=
-function plot_results(times,nodes)
-    nbins = 525
-    data = SNN.bespoke_2dhist(nbins,times,nodes)
-    foreach(normalize!, eachcol(data))
-    Plots.plot(heatmap(data),legend = false, normalize=:pdf)
-    Plots.savefig("untrainedHeatMap_raster_trained.png")
-    SNN.train!(P, C; duration = 3second)
-    SNN.raster(P)
-    Plots.savefig("default_raster_trained.png")
-    (times,nodes) = SNN.get_trains(P)
-    nbins = 525
-    data = SNN.bespoke_2dhist(nbins,times,nodes)
-    foreach(normalize!, eachcol(data))
-    Plots.plot(heatmap(data),legend = false, normalize=:pdf)
-    Plots.savefig("trainingHeatMap_raster_trained.png")
-    print("simulation results plotted")
-end
-=#
-function main()
-    (P,C,times,nodes) = makeNetGetTimes()
-    #plot_results(times,nodes,P)
-end
-main()
+
+(P,C,times,nodes) = makeNetGetTimes()
