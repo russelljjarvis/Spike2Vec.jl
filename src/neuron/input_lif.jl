a41d6e16a0e5790b89ce7761d0bde84b1418d4b9
Russell Jarvis <R.jarvis@westernsydney.edu.au>
2023-03-13 16:51:42 +1100

    ADDING noise free lif

---
+++ src/neuron/input_lif.jl
@@ -0,0 +1,43 @@
+#valgrind --smc-check=all-non-file julia if_net.jl
+
+abstract type AbstractIFParameter end
+@snn_kw struct IFParameter{FT=Float32} <: AbstractIFParameter
+    τm::FT = 20ms
+    τe::FT = 5ms
+    τi::FT = 10ms
+    Vt::FT = -50mV
+    Vr::FT = -60mV
+    El::FT = Vr
+end
+
+abstract type AbstractIF end
+
+@snn_kw mutable struct IF{VFT=Vector{Float32},VBT=Vector{Bool}} <: AbstractIF
+    param::IFParameter = IFParameter()
+    N::Int32 = 100
+    v::VFT = param.Vr #.+ rand(N) .* (param.Vt - param.Vr)
+    ge::VFT = zeros(N)
+    gi::VFT = zeros(N)
+    fire::VBT = zeros(Bool, N)
+    I::VFT = zeros(N)
+    records::Dict = Dict()
+end
+
+"""
+    [Integrate-And-Fire Neuron](https://neuronaldynamics.epfl.ch/online/Ch1.S3.html)
+"""
+IF
+
+function integrate!(p::IF, param::IFParameter, dt::Float32)
+    @unpack N, v, ge, gi, fire, I = p
+    @unpack τm, τe, τi, Vt, Vr, El = param
+    @inbounds for i = 1:N
+        v[i] += dt * (ge[i] + gi[i] - (v[i] - El) + I[i]) / τm
+        ge[i] += dt * -ge[i] / τe
+        gi[i] += dt * -gi[i] / τi
+    end
+    @inbounds for i = 1:N
+        fire[i] = v[i] > Vt
+        v[i] = ifelse(fire[i], Vr, v[i])
+    end
+end
---
+++ src/neuron/noise_free_lif.jl
@@ -0,0 +1,60 @@
+
+abstract type AbstractIFParameter end
+struct IFParameter# <: AbstractIFParameter
+    τm::Float32# = 20ms
+    τe::Float32# = 5ms
+    τi::Float32# = 10ms
+    Vt::Float32# = -50mV
+    Vr::Float32# = -60mV
+    El::Float32# = Vr
+    function IFParameter()
+        new(20.0,5.0,10.0,-50.0,-60.0,-35.0)
+    end
+
+end
+
+abstract type AbstractIF end
+
+struct IFNF <: AbstractIF
+    param::IFParameter
+    N::Int32
+    v::Vector{Float32} 
+    ge::Vector{Float32}
+    gi::Vector{Float32}
+    fire::Vector{Bool}
+    I::Vector{Float32}
+    records::Dict
+    function IFNF(;N::Int64,param::SpikingNeuralNetworks.IFParameter)
+        VFT = Vector{Float32}
+        v::VFT = param.Vr .* ones(N) #.* (param.Vt - param.Vr)
+        ge::VFT = zeros(N)
+        gi::VFT = zeros(N)
+        fire::Vector{Bool} = zeros(Bool, N)
+        I::VFT = zeros(N)
+        records::Dict = Dict()
+        new(param,N,v,ge,gi,fire,I,records)
+    end
+end
+
+"""
+    [Integrate-And-Fire Neuron](https://neuronaldynamics.epfl.ch/online/Ch1.S3.html)
+"""
+IF
+
+function integrate!(p::IFNF, param::IFParameter, dt::Float32)
+    @unpack N, v, ge, gi, fire, I = p
+    @unpack τm, τe, τi, Vt, Vr, El = param
+    @inbounds for i = 1:N
+        v[i] += dt * (ge[i] + gi[i] - (v[i] - El) + I[i]) / τm
+        ge[i] += dt * -ge[i] / τe
+        gi[i] += dt * -gi[i] / τi
+        fire[i] = v[i] > Vt
+        #if fire[i]
+        #    println("fired")
+        #end
+    end
+    @inbounds for i = 1:N
+        v[i] = ifelse(fire[i], Vr, v[i])
+    end
+
+end
