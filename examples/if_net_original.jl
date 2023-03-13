8fc9141e1544a9b983b4387765898be106ba774a
Russell Jarvis <russelljarvis@protonmail.com>
2023-03-13 11:57:30 +1100

    Update if_net_original.jl
    """
    Calculate total maximum number of synapses for 100000EE cells versus 50000II cells
    size_w_one = 50000*50000
    size_w_two = 100000*50000
    size_w_three = 50000*100000
    size_w_four = 100000*100000
    size_w_one+size_w_two+size_w_three+size_w_four
    # 22 500 000 000
    # 22 billion plus (half a billion synpases)
    """

--- examples/if_net_original.jl
+++ examples/if_net_original.jl
@@ -2,19 +2,8 @@ using Plots
 using SpikingNeuralNetworks
 SNN.@load_units
 
-
-#>>> size_w_one = 50000*50000
-#>>> size_w_two = 100000*50000
-#>>> size_w_three = 50000*100000
-#>>> size_w_four = 100000*100000
-#>>> size_w_one+size_w_two+size_w_three+size_w_four
-
-
-# 22 500 000 000
-# 22 billion plus (half a billion)
-
-E = SNN.IF(;N = 120000, param = SNN.IFParameter())#;El = -49mV))
-@time I = SNN.IF(;N = 60000, param = SNN.IFParameter())#;El = -49mV))
+E = SNN.IF(;N = 100000, param = SNN.IFParameter())#;El = -49mV))
+@time I = SNN.IF(;N = 50000, param = SNN.IFParameter())#;El = -49mV))
 EE = SNN.SpikingSynapse(E, E, :ge; σ = 60*0.27/10, p = 0.02)
 EI = SNN.SpikingSynapse(E, I, :ge; σ = 60*0.27/10, p = 0.02)
 IE = SNN.SpikingSynapse(I, E, :gi; σ = -20*4.5/10, p = 0.02)
