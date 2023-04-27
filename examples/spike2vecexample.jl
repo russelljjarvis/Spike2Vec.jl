bee2156e03d6a0e66b5a3b5d317e7949d3eb8838
Russell Jarvis <{ID}+{russelljjarvis}@users.noreply.github.com>
2023-04-27 14:52:32 +1000

    update code

--- examples/spike2vecexample.jl
+++ examples/spike2vecexample.jl
@@ -13,6 +13,9 @@ using ColorSchemes
 using PyCall
 using LinearAlgebra
 using Ridgeline
+using Makie
+using KernelDensity,Distributions
+using CairoMakie#,KernelDensity, Distributions
 #@quickactivate "spike2vec"
 
 initialize_project("spike2vec"; authors="Dr Russell Jarvis", force=true)
@@ -86,7 +89,9 @@ function hide_scope(config)
     (times,nodes) = SNN.get_trains([E,I])
     (times,nodes,E,I)
 end
-
+#tern_it(x,y)= [x + 0.5 * y,sqrt(0.75) *  y]' # convert proportions to cartisian coords on a triangle
+#closest(a,b) = findmin(abs.(a .-b))[2]
+#set_dens(a,b,k) = k.density[closest(a,collect(k.x)),closest(b,collect(k.y))]
 
 function get_plot(times,nodes,division_size)
     step_size = maximum(times)/division_size
@@ -112,21 +117,42 @@ function get_plot(times,nodes,division_size)
     for (ind,_) in enumerate(eachcol(mat_of_distances))
         mat_of_distances[:,ind] = mat_of_distances[:,ind].- mean(mat_of_distances)./std(mat_of_distances)
     end
+    #=
+    fig = nothing
     for (ind,_) in enumerate(eachrow(mat_of_distances))
         n = length(mat_of_distances[1,:])
         θ = LinRange(0, 2pi, n)
         if ind==1
-            fig = plot(θ, mat_of_distances[ind,:], proj=:polar,color=cs1[ind])#, layout = length(mat_of_distances))
+            fig = Plots.plot(θ, mat_of_distances[ind,:], proj=:polar,color=cs1[ind])#, layout = length(mat_of_distances))
         else 
-            plot!(fig,θ,mat_of_distances[ind,:], proj=:polar,color=cs1[ind])  |>display
+            Plots.plot!(fig,θ,mat_of_distances[ind,:], proj=:polar,color=cs1[ind])  |>display
         end
 
     end
+    =#
     #nb %% A slide [code] {"slideshow": {"slide_type": "subslide"}}
-    display(fig)
-    Ridgeline.ridgeline(mat_of_distances)
+    #display(fig)
+
+    f = Figure()
+    Axis(f[1, 1], title = "State visualization",)#yticks = ((1:length(mat_of_distances)) ,String([Char(i) for i in collect(1:length(mat_of_distances))])))
+
+    for (ind,_) in enumerate(eachrow(mat_of_distances))
+        #d = kde(mat_of_distances[ind,:])
+
+        #d = density!(randn(200) .- 2sin((i+3)/6*pi), offset = i / 4,
+        d = Makie.density!(mat_of_distances[ind,:],offset=ind*2,color = :x, colormap = :thermal, colorrange = (-10, 10),strokewidth = 1, strokecolor = :black, bandwidth = 0.02)
+        # this helps with layering in GLMakie
+        #translate!(d, 0, 0, -0.1i)
+    end
+    #f
+
+    #ternKDE = kde(mat_of_distances)
+    #display(f)
+    #current_figure()
+    save("ridgeline.png",f)
+    #println("gets here")
     #savefig("vectors_wrapped.png")
-    return mat_of_distances,fig
+    return mat_of_distances,f
 end
 
 #angles0,distances0,angles1,distances1 = post_proc_viz(mat_of_distances)
@@ -144,13 +170,15 @@ function run_simulation(config)
 
     (times,nodes,E,I) = hide_scope(config)
     @unpack division_size = config
-    @time mat_of_distances,fig = get_plot(times,nodes,division_size)
+    @time (mat_of_distances,f) = get_plot(times,nodes,division_size)
+    display(f)
+
     @time (angles1,distances1) = final_plots(mat_of_distances)
     name = savename(config)
     parse_savename(name)
     #result = simulate(n,w,opt)
 
-    
+    result = (mat_of_distances,fig,f)
     @dict(result)
     safesave(datadir("simulations", savename(config, "sim_results")), (angles1,distances1,fig,nodes,times))
 
