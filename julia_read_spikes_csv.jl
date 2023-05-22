3251cf977f42c759613dd250dd6607878713b88f
Russell Jarvis <rjarvis@westernsydney.edu.au>
2023-05-22 10:47:12 +1000

    local changes

--- julia_read_spikes_csv.jl
+++ julia_read_spikes_csv.jl
@@ -1,18 +1,43 @@
-using PyCall
 using SpikingNeuralNetworks
 using ProgressMeter
 using Revise
+ENV["PYTHON"]="/home/rjjarvis/anaconda3/bin/python"
+#using Pkg
+#Pkg.build("PyCall")
+using PyCall
+using JLD
+
+pyimport("feather")
+println("no read in data")
+
 py"""
 import feather
+#import pandas as pd
 def read():
-    df = feather.read_dataframe("~/git/brian2-sims/sim_data/ai_exp_latest/win2.5_bg60/spikes.feather")
+    df = feather.read_dataframe("spikes.feather")
     return df
 """
 
 df = py"read"();
-times = df.t
-nodes = df.i
-division_size = maximum(times)/10.0
+println("read in data")
+function ragged_to_uniform_col_vec(df)
+    times = df.t
+    nodes = df.i
+    
+    nnn=[];
+    ttt=[];
+    @showprogress for (i, t) in enumerate(times)
+        for tt in t
+            push!(nnn,i);push!(ttt,tt)
+        end
+    end
+    (nnn,ttt)
+end
+
+(nnn,ttt) = ragged_to_uniform_col_vec(df)
+@save "pablos_spikes.jld" nnn ttt
+println("gets here")
+division_size = maximum(ttt)/10.0
 
 function get_plot(times,nodes,division_size)
     step_size = maximum(times)/division_size
@@ -34,48 +59,16 @@ function get_plot(times,nodes,division_size)
     cs1 = ColorScheme(distinguishable_colors(spike_distance_size, transform=protanopic))
     p=nothing
     mat_of_distances ./ norm.(eachcol(mat_of_distances))'
-
- 
-
     for (ind,_) in enumerate(eachcol(mat_of_distances))
         mat_of_distances[:,ind] = mat_of_distances[:,ind].- mean(mat_of_distances)./std(mat_of_distances)
     end
-
- 
-
     f = Figure()
     Axis(f[1, 1], title = "State visualization",)#yticks = ((1:length(mat_of_distances)) ,String([Char(i) for i in collect(1:length(mat_of_distances))])))
-
- 
-
     @showprogress for (ind,_) in enumerate(eachrow(mat_of_distances))
-        #d = kde(mat_of_distances[ind,:])
-
- 
-
-        #d = density!(randn(200) .- 2sin((i+3)/6*pi), offset = i / 4,
         d = Makie.density!(mat_of_distances[ind,:],offset=ind*2,color = :x, colormap = :thermal, colorrange = (-10, 10),strokewidth = 1, strokecolor = :black, bandwidth = 0.02)
-        # this helps with layering in GLMakie
-        #translate!(d, 0, 0, -0.1i)
     end
-    #f
-
- 
-
-    #ternKDE = kde(mat_of_distances)
-    #display(f)
-    #current_figure()
     save("ridgeline.png",f)
-    #println("gets here")
-    #savefig("vectors_wrapped.png")
     return mat_of_distances,f
 end
 
 get_plot(ttt, nnn, division_size[1])
-nnn=[];
-ttt=[];
-for (i, t) in enumerate(times)
-    for tt in t
-        push!(nnn,i);push!(ttt,tt)
-    end
-end
